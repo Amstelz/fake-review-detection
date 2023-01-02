ca20f04d5316694b5fc28d94bc82f0b358507df6
SirapopPCC <sirapopjpt@gmail.com>
2023-01-02 14:06:40 +0700

    re coding

--- Utils/dataframe.py
+++ Utils/dataframe.py
@@ -1,10 +1,14 @@
 import sqlite3
 import pandas as pd 
+import numpy as np
 import re
 from datetime import datetime
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
-
+from pythainlp.tokenize import word_tokenize
+from collections import OrderedDict
+from sklearn.metrics import pairwise_distances
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def load_data():
     conn = sqlite3.connect("../Data/yelpResData.db")
@@ -106,30 +110,146 @@ def onehot(df, col, col_name, type):
     df.drop(col, axis=1, inplace=True)
     return df
 
-def under_sampling(df):
+def under_sampling(df:pd.DataFrame, target:str, big_sample:any,  small_sample:any):
     print("Under-Sampling Data")
 
-    sample_size = len(df[(df['flagged'] == 'Y')])
+    sample_size = len(df[(df[target] == big_sample)])
 
-    authentic_reviews_df = df[df['flagged'] == 'N']
-    fake_reviews_df = df[df['flagged'] == 'Y']
+    small_sample_df = df[df[target] == small_sample]
+    big_sample_df = df[df[target] == big_sample]
 
-    authentic_reviews_us_df = authentic_reviews_df.sample(sample_size)
-    under_sampled_df = pd.concat([authentic_reviews_us_df, fake_reviews_df], axis=0)
+    small_sample_us_df = small_sample_df.sample(sample_size)
+    under_sampled_df = pd.concat([small_sample_us_df, big_sample_df], axis=0)
 
     print("Under-Sampling Complete")
     return under_sampled_df
 
-def over_sampling(df):
+def over_sampling(df:pd.DataFrame, target:str, big_sample:any,  small_sample:any):
     print("Over-Sampling Data")
 
-    sample_size = len(df[(df['flagged'] == 'N')])
+    sample_size = len(df[(df[target] == small_sample)])
 
-    authentic_reviews_df = df[df['flagged'] == 'N']
-    fake_reviews_df = df[df['flagged'] == 'Y']
+    small_sample_df = df[df[target] == small_sample]
+    big_sample_df = df[df[target] == big_sample]
 
-    fake_reviews_os_df = fake_reviews_df.sample(sample_size, replace=True)
-    over_sampled_df = pd.concat([authentic_reviews_df, fake_reviews_os_df], axis=0)
+    big_sample_os_df = big_sample_df.sample(sample_size, replace=True)
+    over_sampled_df = pd.concat([small_sample_df, big_sample_os_df], axis=0)
 
     print("Over-Sampling Complete")
     return over_sampled_df
+
+def feature_engineering_thai(df):
+    mnr_df1 = df[['reviewerID', 'date']].copy()
+    mnr_df2 = mnr_df1.groupby(by=['date', 'reviewerID']).size().reset_index(name='reviewPerDay')
+    mnr_df2['scaledReviewPerDay'] = mnr_df2['reviewPerDay'] / mnr_df2['reviewPerDay'].max()
+    mnr_df2.drop(columns=['reviewPerDay'], inplace=True)
+    df = df.merge(mnr_df2, on=['reviewerID', 'date'], how='inner')
+
+    # Review Length
+    df['reviewsLength'] = df['reviewContent'].apply(
+        lambda x: len(word_tokenize(x, engine="newmm")))
+
+    # Review Deviation
+    df['reviewsDeviation'] = abs(df['rating'] - df['restaurantRating']) / 4
+
+    # Maximum cosine similarity
+    review_data = df
+
+    res = OrderedDict()
+
+    # Iterate over data and create groups of reviewers
+    for row in review_data.iterrows():
+        if row[1].reviewerID in res:
+            res[row[1].reviewerID].append(row[1].reviewContent)
+        else:
+            res[row[1].reviewerID] = [row[1].reviewContent]
+
+    individual_reviewer = [{'reviewerID': k, 'reviewContent': v} for k, v in res.items()]
+    df2 = dict()
+    df2['reviewerID'] = pd.Series([])
+    df2['maximumContentSimilarity'] = pd.Series([])
+    vector = TfidfVectorizer(min_df=0)
+    count = -1
+    for reviewer_data in individual_reviewer:
+        count = count + 1
+        # Handle Null/single review gracefully -24-Apr-2019
+        try:
+            tfidf = vector.fit_transform(reviewer_data['reviewContent'])
+        except:
+            pass
+        cosine = 1 - pairwise_distances(tfidf, metric='cosine')
+
+        np.fill_diagonal(cosine, -np.inf)
+        max = cosine.max()
+
+        # To handle reviewier with just 1 review
+        if max == -np.inf:
+            max = 0
+        df2['reviewerID'][count] = reviewer_data['reviewerID']
+        df2['maximumContentSimilarity'][count] = max
+
+    df3 = pd.DataFrame(df2, columns=['reviewerID', 'maximumContentSimilarity'])
+
+    # left outer join on original datamatrix and cosine dataframe -24-Apr-2019
+    df = pd.merge(review_data, df3, on="reviewerID", how="left")
+
+    df.drop(index=np.where(pd.isnull(df))[0], axis=0, inplace=True)
+    return df
+
+def feature_engineering(df):
+    mnr_df1 = df[['reviewerID', 'date']].copy()
+    mnr_df2 = mnr_df1.groupby(by=['date', 'reviewerID']).size().reset_index(name='reviewPerDay')
+    mnr_df2['scaledReviewPerDay'] = mnr_df2['reviewPerDay'] / mnr_df2['reviewPerDay'].max()
+    mnr_df2.drop(columns=['reviewPerDay'], inplace=True)
+    df = df.merge(mnr_df2, on=['reviewerID', 'date'], how='inner')
+
+    # Review Length
+    df['reviewsLength'] = df['reviewContent'].apply(
+        lambda x: len(x.split()))
+
+    # Review Deviation
+    df['reviewsDeviation'] = abs(df['rating'] - df['restaurantRating']) / 4
+
+    # Maximum cosine similarity
+    review_data = df
+
+    res = OrderedDict()
+
+    # Iterate over data and create groups of reviewers
+    for row in review_data.iterrows():
+        if row[1].reviewerID in res:
+            res[row[1].reviewerID].append(row[1].reviewContent)
+        else:
+            res[row[1].reviewerID] = [row[1].reviewContent]
+
+    individual_reviewer = [{'reviewerID': k, 'reviewContent': v} for k, v in res.items()]
+    df2 = dict()
+    df2['reviewerID'] = pd.Series([])
+    df2['maximumContentSimilarity'] = pd.Series([])
+    vector = TfidfVectorizer(min_df=0)
+    count = -1
+    for reviewer_data in individual_reviewer:
+        count = count + 1
+        # Handle Null/single review gracefully -24-Apr-2019
+        try:
+            tfidf = vector.fit_transform(reviewer_data['reviewContent'])
+        except:
+            pass
+        cosine = 1 - pairwise_distances(tfidf, metric='cosine')
+
+        np.fill_diagonal(cosine, -np.inf)
+        max = cosine.max()
+
+        # To handle reviewier with just 1 review
+        if max == -np.inf:
+            max = 0
+        df2['reviewerID'][count] = reviewer_data['reviewerID']
+        df2['maximumContentSimilarity'][count] = max
+
+    df3 = pd.DataFrame(df2, columns=['reviewerID', 'maximumContentSimilarity'])
+
+    # left outer join on original datamatrix and cosine dataframe -24-Apr-2019
+    df = pd.merge(review_data, df3, on="reviewerID", how="left")
+
+    df.drop(index=np.where(pd.isnull(df))[0], axis=0, inplace=True)
+    return df
---
+++ Utils/learning.py
@@ -0,0 +1,154 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+from sklearn.model_selection import GridSearchCV
+
+def plot_confusion_matrix(y_true, y_pred, classes, title=None, cmap=plt.cm.Blues):
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes,
+           yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+
+    return plt
+
+def supervised_learning(df, model, algorithm, drop_columns: list = None, target_column: str = None):
+    df = df.copy()
+    print("Training "+algorithm+" Model")
+    labels = df[target_column]
+
+    df.drop(drop_columns, axis=1, inplace=True)
+
+    train_data, test_data, train_label, test_label = train_test_split(df, labels, test_size=0.25, random_state=42)
+
+    # param_grid = {
+    #     'n_estimators': [10, 500],
+    #     'max_features': ['auto', 'sqrt', 'log2'],
+    #     'max_depth': [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    #     'criterion': ['gini', 'entropy']
+    # }
+    # grid_clf_acc = GridSearchCV(estimator=model, param_grid=param_grid, cv=5)
+    #
+    # grid_clf_acc.fit(train_data, train_label)
+
+    model.fit(train_data, train_label)
+    predicted_labels = model.predict(test_data)
+
+    # print('Best Params : ', grid_clf_acc.best_params_)
+    print(algorithm + ' Model Results')
+    print('--' * 20)
+    print('Accuracy Score : ' + str(accuracy_score(test_label, predicted_labels)))
+    print('Precision Score : ' + str(precision_score(test_label, predicted_labels, pos_label="Y")))
+    print('Recall Score : ' + str(recall_score(test_label, predicted_labels, pos_label="Y")))
+    print('F1 Score : ' + str(f1_score(test_label, predicted_labels, pos_label="Y")))
+    print('Confusion Matrix : \n' + str(confusion_matrix(test_label, predicted_labels)))
+    plot_confusion_matrix(test_label, predicted_labels, classes=['N', 'Y'],
+                          title=algorithm + ' Confusion Matrix').show()
+
+    return test_label, predicted_labels, model
+
+def semi_supervised_learning(df, model, algorithm, threshold=0.8, iterations=40, drop_columns: list = None, target_column: str = None):
+    df = df.copy()
+    print("Training "+ algorithm +" Model")
+    labels = df[target_column]
+
+    df.drop(drop_columns, axis=1, inplace=True)
+
+    train_data, test_data, train_label, test_label = train_test_split(df, labels, test_size=0.25, random_state=42)
+
+    test_review_content = test_data['reviewContent']
+    train_data.drop(['reviewContent'], axis=1, inplace=True)
+    test_data.drop(['reviewContent'], axis=1, inplace=True)
+    
+    test_data_copy = test_data.copy()
+    test_label_copy = test_label.copy()
+
+    all_labeled = False
+
+    current_iteration = 0
+
+    # param_grid = {
+    #     'n_estimators': [10, 500],
+    #     'max_features': ['auto', 'sqrt', 'log2'],
+    #     'max_depth': [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    #     'criterion': ['gini', 'entropy']
+    # }
+    # grid_clf_acc = GridSearchCV(estimator=model, param_grid=param_grid, cv=5)
+    #
+    # grid_clf_acc.fit(train_data, train_label)
+    feature = list(train_data.columns)
+    
+    pbar = tqdm(total=iterations)
+
+    while not all_labeled and (current_iteration < iterations):
+        # print("Before train data length : ", len(train_data))
+        # print("Before test data length : ", len(test_data))
+        current_iteration += 1
+        model.fit(train_data, train_label)
+
+        probabilities = model.predict_proba(test_data)
+        pseudo_labels = model.predict(test_data)
+
+        indices = np.argwhere(probabilities > threshold)
+
+        # print("rows above threshold : ", len(indices))
+        for item in indices:
+            train_data.loc[test_data.index[item[0]]] = test_data.iloc[item[0]]
+            train_label.loc[test_data.index[item[0]]] = pseudo_labels[item[0]]
+        test_data.drop(test_data.index[indices[:, 0]], inplace=True)
+        test_label.drop(test_label.index[indices[:, 0]], inplace=True)
+        # print("After train data length : ", len(train_data))
+        # print("After test data length : ", len(test_data))
+        print("--" * 20)
+
+        if len(test_data) == 0:
+            print("Exiting loop")
+            all_labeled = True
+        pbar.update(1)
+    pbar.close()
+    predicted_labels = model.predict(test_data_copy)
+
+    # print('Best Params : ', grid_clf_acc.best_params_)
+    print(algorithm + ' Model Results')
+    print('--' * 20)
+    print('Accuracy Score : ' + str(accuracy_score(test_label_copy, predicted_labels)))
+    print('Precision Score : ' + str(precision_score(test_label_copy, predicted_labels, pos_label="Y")))
+    print('Recall Score : ' + str(recall_score(test_label_copy, predicted_labels, pos_label="Y")))
+    print('F1 Score : ' + str(f1_score(test_label_copy, predicted_labels, pos_label="Y")))
+    print('Confusion Matrix : \n' + str(confusion_matrix(test_label_copy, predicted_labels)))
+    plot_confusion_matrix(test_label_copy, predicted_labels, classes=['N', 'Y'],
+                          title=algorithm + ' Confusion Matrix').show()
+
+    results = test_data_copy.copy()
+    results['reviewContent'] = test_review_content
+    results[target_column] = test_label_copy
+    results['predicted'] = predicted_labels
+
+    return model, results, feature
+                          
